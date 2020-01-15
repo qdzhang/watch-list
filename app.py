1032e5c3d903f61a664f6b19d241c693037ca47a
qidongz <qdzhangcn@gmail.com>
2020-01-15 11:38:11 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Welcome!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route("/user/<name>")
 def user_page(name):
     return 'Users: %s' % name
+
+
+name = 'Mike'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
